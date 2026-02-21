a6197629e0f28931cf201c2cfe0fc67e439977fe
mriana9 <mariana.algafy@sharkod>
2026-02-21 13:39:15 +0200

    Initial commit: fix h1

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,11 @@ function App() {
 
   return (
     <>
-
+     
+      <h2 className="text-5xl md:text-6xl font-extrabold text-gray-900 leading-tight">
+          Build Your Dream Website <br />
+          <span className="text-indigo-600">Without Writing Code</span>
+        </h2>
     </>
   )
 }
